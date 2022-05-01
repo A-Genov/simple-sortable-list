748f0d91a12699b10c6cae2b04743e067ba484e0
A-Genov <80156452+A-Genov@users.noreply.github.com>
2022-05-01 13:55:56 +0300

    drag & drop functionality

--- script.js
+++ script.js
@@ -42,4 +42,57 @@ function createList() {
             listItems.push(listItem);
             draggableList.appendChild(listItem);
         });
+
+        addEventListeners();
+}
+
+function dragStart() {
+    // console.log('dragstart');
+    dragStartIndex = Number(this.closest('li').getAttribute('data-index'));
+}
+
+function dragEnter() {
+    // console.log('dragenter');
+    this.classList.add('over');
+}
+
+function dragLeave() {
+    // console.log('dragleave');
+    this.classList.remove('over');
+}
+
+function dragOver(e) {
+    // console.log('dragover');
+    e.preventDefault();
+}
+
+function dragDrop() {
+    // console.log('drop');
+    const dragEndIndex = Number(this.getAttribute('data-index'));
+    swapItems(dragStartIndex, dragEndIndex);
+    this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart)
+    })
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver)
+        item.addEventListener('drop', dragDrop)
+        item.addEventListener('dragenter', dragEnter)
+        item.addEventListener('dragleave', dragLeave)
+    })
 }
